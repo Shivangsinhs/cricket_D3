c672c6a7904a1dee60edde73f717d54c94098777
Shivang Sinha <116307753+Shivangsinhs@users.noreply.github.com>
2023-03-31 12:06:43 +0100

    Add files via upload

---
+++ IPL UPDATE/titleWinner.js
@@ -0,0 +1,103 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var yearScale = d3.scaleLinear()
+    .domain([2008,2022]).range([20,1150]);
+
+var svg = d3.select('#titleWinner').append('svg').attr('width', 1200).attr('height', 300);
+
+
+svg.append('text')
+    .attr('class', 'title')
+    .attr('transform','translate(550,30)')
+    .style('font-weight', 'bold')
+    .text('IPL Title Winners');
+
+
+var yearScale = d3.scaleLinear()
+    .domain([2008,2022]).range([50,width]);
+
+
+d3.csv("ipl_data.csv").then((data) => {
+    let g = svg.append("g").selectAll('g')
+        .data(data)
+        .enter()
+        .append('g').attr('class', (d)=>`x axis y${d.year}`)
+        .attr('id', (d)=>d.year)
+        .attr('class', `title`)
+        .attr('transform', (d, i)=> `translate(${yearScale(d.year)},70)`)
+        .on("mouseover", function(d,i) {
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr("transform", (d)=> `translate(${yearScale(Number(d.year))}, 60) scale(1.2)`);
+        })
+        .on("mouseleave", function(d,i) {
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr("transform", (d)=> `translate(${yearScale(Number(d.year))}, 70) scale(1)`);
+        });
+
+        g.append('text')
+            .attr("x", 10)
+            .attr("y", 140)
+            .text((d)=>d.year);
+
+        g.append('text')
+            .style('fill', 'black')
+            .style('font-size', '10px')
+            .style('text-anchor', 'middle')
+            .attr("x", 25)
+            .attr("y", (d, i)=> [1, 14].includes(i) ? 70 : 60)
+            .text((d)=>d.winner).call(wrap,30);
+
+        g.append("image")
+            .attr('href', function(d){ return d.image; })
+            .attr('id', function(d){ return d.year; })            
+            .attr("width", "50px")
+            .attr("height", "50px");
+        
+        g.append('line')
+            .attr('x1', 0)
+            .attr('y1', 160)
+            .attr('x2', 50)
+            .attr('y2', 160)
+            .attr("stroke-width","5")
+            .attr("stroke",(d)=> d.year == 2022 ? "#4682b4" : '')
+
+})
+
+function wrap(text, width) {
+    text.each(function () {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            x = text.attr("x"),
+            y = text.attr("y"),
+            dy = 0, //parseFloat(text.attr("dy")),
+            tspan = text.text(null)
+                        .append("tspan")
+                        .attr("x", x)
+                        .attr("y", y)
+                        .attr("dy", dy + "em");
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan")
+                            .attr("x", x)
+                            .attr("y", y)
+                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                            .text(word);
+            }
+        }
+    });
+}
---
+++ IPL UPDATE/topScorrer.js
@@ -0,0 +1,123 @@
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+    
+let year = '2022';
+
+var x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.1);
+var y = d3.scaleLinear()
+      .range([height, 0]);
+
+function scalex(sl){ 
+    return x(sl)+35;
+}
+
+function scaley(score){ 
+    return y(score)+15;
+}
+var yearScale = d3.scaleLinear()
+    .domain([2008,2022]).range([50,width]);
+
+          
+var topScorrer = d3.select("#topScorrer").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+var topWickets = d3.select("#topWickets").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", 
+      "translate(" + margin.left + "," + margin.top + ")");
+      
+
+d3.csv("top10Batsman.csv").then(function(data) {
+
+  function init(data, type= 'most_runs') {
+      x.domain(data.map(function(d) { return d.players_name; }));
+      y.domain([0, d3.max(data, function(d) { return d[type]; })]);
+      topScorrer.selectAll('*').remove();
+      let bars = topScorrer.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.players_name); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d[type]); })
+        .attr("height", function(d) { return height - y(d[type]); })
+        .style('fill', 'steelblue');
+
+      topScorrer.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      topScorrer.append("g")
+          .call(d3.axisLeft(y));
+    }
+
+
+
+
+    let dataByYear = data.filter((e)=> e.year == year); 
+    init(dataByYear, 'most_runs');
+
+         
+
+    d3.csv("top10Wickets.csv").then(function(dataWicket) {
+      function initWicket(data) {
+          x.domain(data.map(function(d) { return d.players_name; }));
+          y.domain([0, d3.max(data, function(d) { return d.most_wickets; })]);
+          topWickets.selectAll('*').remove();
+          topWickets.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.players_name); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.most_wickets); })
+            .attr("height", function(d) { return height - y(d.most_wickets); })
+            .style('fill', 'steelblue');
+
+            topWickets.append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(d3.axisBottom(x));
+
+              topWickets.append("g")
+              .call(d3.axisLeft(y));
+        }
+
+        initWicket(dataWicket.filter((e)=> e.year == year, 'most_wickets'));
+
+
+        svg.selectAll('.title').on("click", function(d) {
+          d3.selectAll('line').remove()
+                
+          d3.select(this)
+                .append('line')
+                .attr('x1', 0)
+                .attr('y1', 160)
+                .attr('x2', 50)
+                .attr('y2', 160)
+                .attr("stroke-width","5")
+                .attr("stroke","#4682b4")
+
+          init(data.filter((e)=> e.year == this.id));
+          initWicket(dataWicket.filter((e)=> e.year == this.id));
+        })
+
+    });
+
+
+   
+
+});
+
+
+
+     
